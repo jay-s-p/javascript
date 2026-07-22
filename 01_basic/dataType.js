033abaa6a55bed7932188f80e15f16c5cdcef9a9
jay singh <jaysinghmd@gmail.com>
2026-07-22 04:39:47 +0000

    datatype

---
+++ 01_basic/dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3+3) ; console.log("jay") //not standard way to write code
+
+let name= "jay";
+let age =122
+let isLoggedIn= true;
+
+// number => 2 to power 53
+// BigIntt
+// string =>""
+// boolean => true or false
+// null => standalone value => its a value given as null (empty)
+// undefined => not assigned
+// symbol=> unique
+
+//  object;
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof name) //string
