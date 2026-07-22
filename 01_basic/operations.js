c092e59efae33ea2422a82c08df04caa6f0c44f2
jay singh <jaysinghmd@gmail.com>
2026-07-22 15:42:57 +0000

    operation

---
+++ 01_basic/operations.js
@@ -0,0 +1,37 @@
+let value =3
+let negativeValue= -value
+console.log (negativeValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " jay"
+
+let str3 = str1 + str2  //concatnation
+console.log(str3); //hello jay
+
+console.log("1" + 2); //12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2");  //32
+
+console.log( (3 + 4) * 5 % 3); 
+
+console.log(+true); // 1 converted booleAN In number 1
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // not recommended to write in company
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
